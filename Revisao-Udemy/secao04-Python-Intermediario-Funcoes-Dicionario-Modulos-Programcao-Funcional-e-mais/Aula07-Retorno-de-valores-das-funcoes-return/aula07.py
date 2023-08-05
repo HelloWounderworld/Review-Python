f712e9d2d61a5e8b7fe58cfa19d0dc504c82a5c3
Leonardo Takashi Teramatsu <vercel@buudigital.com>
2023-08-05 20:05:49 -0300

    Atualizando a minha nota

--- Revisao-Udemy/secao04-Python-Intermediario-Funcoes-Dicionario-Modulos-Programcao-Funcional-e-mais/Aula07-Retorno-de-valores-das-funcoes-return/aula07.py
+++ Revisao-Udemy/secao04-Python-Intermediario-Funcoes-Dicionario-Modulos-Programcao-Funcional-e-mais/Aula07-Retorno-de-valores-das-funcoes-return/aula07.py
@@ -18,3 +18,15 @@ def sum(x,y):
 
 x = sum(1,2)
 print(x)
+
+'''
+Quantidade de números inteiros ímpares com três algarismos distintos entre 100 e 999.
+'''
+
+count = 0
+for i in range(100, 1000):
+    if i%2==1:
+        j = str(i)
+        if (int(j[0]) != int(j[1])) and (int(j[0]) != int(j[2])) and (int(j[1]) != int(j[2])):
+            count+=1
+print(count)
