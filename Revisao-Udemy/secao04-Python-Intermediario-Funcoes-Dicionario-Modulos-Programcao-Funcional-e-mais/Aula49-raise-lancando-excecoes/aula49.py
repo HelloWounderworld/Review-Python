878cde3439bb2829ff041272113cbe745c3c1da2
Leonardo Takashi Teramatsu <ltakashi@minsait.com>
2023-09-27 11:35:28 -0300

    Atualizando a minha nota!

--- Revisao-Udemy/secao04-Python-Intermediario-Funcoes-Dicionario-Modulos-Programcao-Funcional-e-mais/Aula49-raise-lancando-excecoes/aula49.py
+++ Revisao-Udemy/secao04-Python-Intermediario-Funcoes-Dicionario-Modulos-Programcao-Funcional-e-mais/Aula49-raise-lancando-excecoes/aula49.py
@@ -3,10 +3,22 @@
 
 def divisao_por_zero_indefinido(denominador):
     if denominador == 0:
-        raise ZeroDivisionError('Você está dividindo por zero!')
+        raise ZeroDivisionError('Não pode dividir pelo zero!')
+
+def deve_ser_int_ou_float(n):
+    tipo_n = type(n)
+    if not isinstance(n, (float, int)):
+        raise TypeError(
+            f'"{n}" deve ser int ou float. '
+            f'"{tipo_n.__name__}" enviado.'
+        )
+    return True
 
 def divide(x, y):
+    deve_ser_int_ou_float(x)
+    deve_ser_int_ou_float(y)
+    
     divisao_por_zero_indefinido(y)
     return x / y
 
-print(divide(8, 0))
+print(divide(8, '0'))
--- Revisao-Udemy/secao04-Python-Intermediario-Funcoes-Dicionario-Modulos-Programcao-Funcional-e-mais/Aula50-Modulos-import-from-as-e-asterisco/aula50-prof.py
+++ Revisao-Udemy/secao04-Python-Intermediario-Funcoes-Dicionario-Modulos-Programcao-Funcional-e-mais/Aula50-Modulos-import-from-as-e-asterisco/aula50-prof.py
@@ -3,13 +3,18 @@
 # inteiro - import nome_modulo
 # Vantagens: você tem o namespace do módulo
 # Desvantagens: nomes grandes
-#import sys
+# import sys
 
-#platform = 'A minha'
-#print(sys.platform)
-#print(platform)
-#print('Hello WounderWorld!')
-#sys.exit()
+# platform = 'A MINHA'
+# print(sys.platform)
+# print(platform)
+
+# partes - from nome_modulo import objeto1, objeto2
+# Vantagens: nomes pequenos
+# Desvantagens: Sem o namespace do módulo
+# from sys import exit, platform
+
+# print(platform)
 
 # alias 1 - import nome_modulo as apelido
 # import sys as s
@@ -18,27 +23,20 @@
 # print(s.platform)
 # print(sys)
 
-# partes - from nome_modulo import objeto1, objeto2
-# Vantagens: nomes pequenos
-# Desvantagens: Sem o namespace do módulo
-#from sys import exit, platform
 
-#platform = 'A minha'
-#print(platform)
-#exit()
+# alias 2 - from nome_modulo import objeto as apelido
+# from sys import exit as ex
+# from sys import platform as pf
 
-from sys import exit as ex, platform as pt
-
-platform = 'A minha'
-exit = 'Sair'
-print(pt)
-print(platform)
-print(exit)
-ex()
+# print(pf)
 
 # Vantagens: você pode reservar nomes para seu código
 # Desvantagens: pode ficar fora do padrão da linguagem
 
 # má prática - from nome_modulo import *
 # Vantagens: importa tudo de um módulo
-# Desvantagens: importa tudo de um módul
+# Desvantagens: importa tudo de um módulo
+# from sys import exit, platform
+
+# print(platform)
+# exit()
