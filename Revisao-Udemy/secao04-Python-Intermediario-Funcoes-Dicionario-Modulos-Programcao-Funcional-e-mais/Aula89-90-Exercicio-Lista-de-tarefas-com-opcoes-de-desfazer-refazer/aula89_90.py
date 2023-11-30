0e13f082a64b45a801031f1037e3936a23c5c93c
Leonardo Takashi Teramatsu <vercel@buudigital.com>
2023-11-29 21:41:58 -0300

    Atualizando a minha nota

--- Revisao-Udemy/secao04-Python-Intermediario-Funcoes-Dicionario-Modulos-Programcao-Funcional-e-mais/Aula89-90-Exercicio-Lista-de-tarefas-com-opcoes-de-desfazer-refazer/aula89_90.py
+++ Revisao-Udemy/secao04-Python-Intermediario-Funcoes-Dicionario-Modulos-Programcao-Funcional-e-mais/Aula89-90-Exercicio-Lista-de-tarefas-com-opcoes-de-desfazer-refazer/aula89_90.py
@@ -8,3 +8,42 @@
 # desfazer = [] -> Refazer ['caminhar', 'fazer café']
 # refazer = todo ['fazer café']
 # refazer = todo ['fazer café', 'caminhar']
+import os
+
+def listar(argumento, lista=None):
+    if lista is None:
+        lista = []
+
+    lista.append(argumento)
+    return lista
+
+def desfazer(lista=None):
+    if lista is None:
+        lista = []
+
+    lista.pop()
+    return lista
+
+def refazer(listaRefaz=None, lista=None):
+    if lista is None:
+        lista = []
+
+    lista.append(listaRefaz[0])
+    listaRefaz.pop(0)
+    return lista
+
+guardando_sigla = {'listar': 0, 'desfazer': 1, 'refazer': 2}
+
+lista = []
+listaRefaz = []
+
+while (True):
+    entrada = input("Comnados: listar, desfazer, refazer\n Digite uma tarefa ou comando: ")
+    if guardando_sigla[entrada] == 0:
+        ...
+    elif guardando_sigla[entrada] == 1:
+        ...
+    elif guardando_sigla[entrada] == 2:
+        ...
+    else:
+        print(listar(entrada, lista))
