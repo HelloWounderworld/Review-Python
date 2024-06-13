a1df63ec7c5ebf31cade9c27a99b6227ea94a25e
Leonardo Takashi Teramatsu <leonardo.teramatsu@gmail.com>
2024-06-13 11:37:16 +0900

    Secao 05 - Aula 31 - LogFileMixin, manipulando path e marcando log

---
+++ Revisao-Udemy/secao05-Introducao-a-programcao-orientada-a-objetos-POO-em-Python/Aula31-Parte3-LogFileMixin-uniao-de-tudo-ate-aqui/mixinrecordlogs/log.py
@@ -0,0 +1,40 @@
+# Abstração
+# Herança - é um
+from pathlib import Path
+
+print("Caminho: ", Path(__file__).parent)
+LOG_FILE = Path(__file__).parent / 'log.txt'
+# print(LOG_FILE)
+
+
+class Log:
+    def _log(self, msg):
+        raise NotImplementedError('Implemente o método log')
+    
+    def log_error(self, msg):
+        return self._log(f'Error: {msg}')
+    
+    def log_success(self, msg):
+        return self._log(f'Success: {msg}')
+
+class LogFileMixin(Log):
+    def _log(self, msg):
+        msg_fomatada = f'{msg} ({self.__class__.__name__})'
+        print('Salvando no log:', msg_fomatada)
+        with open(LOG_FILE, 'a') as arquivo:
+            arquivo.write(msg_fomatada)
+            arquivo.write('\n')
+            arquivo.flush()
+
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
+
+if __name__ == '__main__':
+    lp = LogPrintMixin()
+    lp.log_error('qualquer coisa')
+    lp.log_success('Que legal')
+    print()
+    lf = LogFileMixin()
+    lf.log_error('qualquer coisa')
+    lf.log_success('Que legal')
