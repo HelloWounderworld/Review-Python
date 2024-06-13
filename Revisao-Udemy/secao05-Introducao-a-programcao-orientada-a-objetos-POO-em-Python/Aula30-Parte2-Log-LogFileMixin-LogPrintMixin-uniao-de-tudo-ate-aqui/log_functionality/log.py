fe340378ba42a67d4617c8e560872e23b20faa3f
Leonardo Takashi Teramatsu <leonardo.teramatsu@gmail.com>
2024-06-13 11:28:15 +0900

    Secao 05 - Aula 30 - LogFileMixin e LogPrintMixin

--- Revisao-Udemy/secao05-Introducao-a-programcao-orientada-a-objetos-POO-em-Python/Aula30-Parte2-Log-LogFileMixin-LogPrintMixin-uniao-de-tudo-ate-aqui/log_functionality/log.py
+++ Revisao-Udemy/secao05-Introducao-a-programcao-orientada-a-objetos-POO-em-Python/Aula30-Parte2-Log-LogFileMixin-LogPrintMixin-uniao-de-tudo-ate-aqui/log_functionality/log.py
@@ -1,6 +1,7 @@
 # Abstração
 # Herança - é um
 class Log:
+    # Nao pode ser instanciado diretamente.
     def _log(self, msg):
         raise NotImplementedError('Implemente o método log')
 
@@ -12,8 +13,18 @@ class Log:
 
 
 class LogFileMixin(Log):
+    def __init__(self, filename):
+        self.log_file = open(filename, 'a')
+
+    def log(self, message):
+        self.log_file.write(f'{message}  ({self.__class__.__name__})' + '\n')
+        self.log_file.flush()
+
+    def close_log(self):
+        self.log_file.close()
+
     def _log(self, msg):
-        print(msg)
+        print(f'{msg} ({self.__class__.__name__})')
 
 
 class LogPrintMixin(Log):
@@ -22,8 +33,13 @@ class LogPrintMixin(Log):
 
 
 if __name__ == '__main__':
-    l = LogFileMixin()
-    l.log('qualquer coisa')
-    l = LogPrintMixin()
-    l.log_error('qualquer coisa')
-    l.log_success('Que legal')
+    # l1 = LogFileMixin('qualquer')
+    # l1._log('qualquer coisa')
+    # print()
+    l2 = LogPrintMixin()
+    l2.log_error('qualquer coisa')
+    l2.log_success('Que legal')
+    print()
+    l3 = LogFileMixin("c:\\Users\\leona\\Documents\\study-programming\\Review-Python\\Revisao-Udemy\\secao05-Introducao-a-programcao-orientada-a-objetos-POO-em-Python\\Aula30-Parte2-Log-LogFileMixin-LogPrintMixin-uniao-de-tudo-ate-aqui\\log_functionality\\log.txt")
+    l3.log('qualquer coisa')
+    l3.close_log()
